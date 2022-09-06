c0fb854702bb73b53a17766bf45cd052011279b2
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-06 23:02:28 +0600

    3.2.3 final version

--- Hello Kotlin/src/Hello.kt
+++ Hello Kotlin/src/Hello.kt
@@ -90,3 +90,32 @@ fun main(args: Array<String>) {
 //         "Friday", "Saturday", "Sunday")
 //     return week[java.util.Random().nextInt(week.size)]
 // }
+
+// 3.2.3
+
+// fun randomDay() : String {
+//     val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+//         "Friday", "Saturday", "Sunday")
+//     return week[Random().nextInt(week.size)]
+// }
+
+// fun fishFood (day : String) : String {
+//     return when (day) {
+//         "Monday" -> "flakes"
+//         "Wednesday" -> "redworms"
+//         "Thursday" -> "granules"
+//         "Friday" -> "mosquitoes"
+//         "Sunday" -> "plankton"
+//         else -> "nothing"
+//     }
+// }
+
+// fun feedTheFish() {
+//     val day = randomDay()
+//     val food = fishFood(day)
+//     println ("Today is $day and the fish eat $food")
+// }
+
+// fun main(args: Array<String>) {
+//     feedTheFish()
+// }
