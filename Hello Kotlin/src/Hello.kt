ad177820436df326558f70447b02abdf0c76d508
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-06 23:13:15 +0600

    4.2

--- Hello Kotlin/src/Hello.kt
+++ Hello Kotlin/src/Hello.kt
@@ -133,3 +133,46 @@ fun main(args: Array<String>) {
 //     swim("slow")
 //     swim(speed = "turtle-like")
 // }
+
+// 4.2
+// import java.util.*
+// fun swim(speed : String = "fast") {
+//     println("swimming $speed")
+// }
+
+// fun main(args: Array<String>) {
+//     feedTheFish()
+//     swim()
+//     swim("slow")
+//     swim(speed = "turtle-like")
+// }
+// fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+//     return when {
+//         temperature > 30 -> true
+//         dirty > 30 -> true
+//         day == "Sunday" ->  true
+//         else -> false
+//     }
+// }
+
+// fun feedTheFish() {
+//     val day = randomDay()
+//     val food = fishFood(day)
+//     println ("Today is $day and the fish eat $food")
+//     println("Change water: ${shouldChangeWater(day)}")
+// }
+//  fun randomDay() : String {
+//      val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+//          "Friday", "Saturday", "Sunday")
+//      return week[Random().nextInt(week.size)]
+//  }
+//  fun fishFood (day : String) : String {
+//      return when (day) {
+//          "Monday" -> "flakes"
+//          "Wednesday" -> "redworms"
+//          "Thursday" -> "granules"
+//          "Friday" -> "mosquitoes"
+//          "Sunday" -> "plankton"
+//          else -> "nothing"
+//      }
+//  }
