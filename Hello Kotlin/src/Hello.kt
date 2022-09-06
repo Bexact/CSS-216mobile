2caba53e8d0e1e34f9c9d86fd7dfec9937ceb52b
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-06 22:31:58 +0600

    2

--- Hello Kotlin/src/Hello.kt
+++ Hello Kotlin/src/Hello.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
     println("Hello, ${args[0]}")
 }
+//2.1
+//fun main(args: Array<String>) {
+//    val isUnit = println("This is an expression")
+//    println(isUnit)
+//}
+//2.3 2.4
+//fun main(args: Array<String>) {
+//    val temperature = 10
+//    val isHot = if (temperature > 50) true else false
+//    println(isHot)
+//}
+//2.5
+// fun main(args: Array<String>) {
+//     val temperature = 10
+//     val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+//     println(message)
+// }
