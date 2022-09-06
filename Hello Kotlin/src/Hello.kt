d5eb3c856d5549eb131c7a58ac66a67f310ae86d
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-06 22:54:15 +0600

    3.1

--- Hello Kotlin/src/Hello.kt
+++ Hello Kotlin/src/Hello.kt
@@ -18,3 +18,18 @@ fun main(args: Array<String>) {
 //     val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
 //     println(message)
 // }
+// 3.1
+// fun feedTheFish() {
+//     val day = randomDay()
+//     val food = "pellets"
+//     println ("Today is $day and the fish eat $food")
+// }
+
+// fun main(args: Array<String>) {
+//     feedTheFish()
+// }
+// fun randomDay(): String {
+//     var week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday",
+//         "Friday", "Saturday", "Sunday")
+//     return week[java.util.Random().nextInt(week.size)]
+// }
