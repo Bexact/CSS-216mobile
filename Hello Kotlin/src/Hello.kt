72705f5f43f72e7f9c5cd544f5750daeaddfe49a
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-07 14:34:52 +0600

    5.5

--- Hello Kotlin/src/Hello.kt
+++ Hello Kotlin/src/Hello.kt
@@ -269,3 +269,15 @@ fun main(args: Array<String>) {
 //     println("-----")
 //     println("all: ${lazyMap.toList()}")
 // }
+
+// 5.5
+
+// fun main() {
+//     val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+//     val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+//         println("access: $it")
+//         it
+//     }
+//     println("-----")
+//     println("filtered: ${lazyMap2.toList()}")
+// }
