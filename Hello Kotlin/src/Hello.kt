81e9516a676352376790572ae27aa5c4057daeba
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-07 14:35:20 +0600

    5.6

--- Hello Kotlin/src/Hello.kt
+++ Hello Kotlin/src/Hello.kt
@@ -281,3 +281,14 @@ fun main(args: Array<String>) {
 //     println("-----")
 //     println("filtered: ${lazyMap2.toList()}")
 // }
+
+// 5.6
+
+// fun main() {
+//     val mysports = listOf("basketball", "fishing", "running")
+//     val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+//     val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+//     val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+//     println("-----")
+//     println("Flat: ${mylist.flatten()}")
+// }
