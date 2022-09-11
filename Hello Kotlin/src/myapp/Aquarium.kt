52536b8aa0f1cfff363b3c8db60633d4cfc2c919
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-11 22:22:47 +0600

    5.1

--- Hello Kotlin/src/myapp/Aquarium.kt
+++ Hello Kotlin/src/myapp/Aquarium.kt
@@ -1,6 +1,6 @@
 package example.myapp
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40){
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40){
     init {
         println("aquarium initializing")
     }
@@ -18,14 +18,23 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
 //          val volume: Int
 //         get() = width * height * length / 1000  
 //     3.5
-    var volume: Int
+   open var volume: Int
     get() = width * height * length / 1000
     set(value) {
         height = (value * 1000) / (width * length)
     }
+//     5.1
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
     
-    fun printSize(){
-        println("Width: $width cm " + "Length: $length cm " + "Height: $height cm ")
-          println("Volume: $volume litres")
+    fun printSize() {
+        println(shape)
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
 }
