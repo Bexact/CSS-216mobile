da334af4c6d8f04320169a8e1f4556e7ea3c5b1c
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-11 21:41:45 +0600

    3.2

--- Hello Kotlin/src/myapp/Aquarium.kt
+++ Hello Kotlin/src/myapp/Aquarium.kt
@@ -1,7 +1,12 @@
 package example.myapp
 
 class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40){
-
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        println("Volume: ${width * length * height / 1000} liters")
+    }
     fun printSize(){
         println("Width: $width cm " + "Length: $length cm " + "Height: $height cm ")
     }
