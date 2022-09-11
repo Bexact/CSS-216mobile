c4c27f3767191aa5d16a4ac6ee36c04e23fdc48d
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-11 22:03:31 +0600

    3.4

--- Hello Kotlin/src/myapp/Aquarium.kt
+++ Hello Kotlin/src/myapp/Aquarium.kt
@@ -4,9 +4,9 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     init {
         println("aquarium initializing")
     }
-    init {
-        println("Volume: ${width * length * height / 1000} liters")
-    }
+//    3.4 init {
+//         println("Volume: ${width * length * height / 1000} liters")
+//     }
 //     3.3
        constructor(numberOfFish: Int) : this() {
 
@@ -14,7 +14,11 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
 
         height = (tank / (length * width)).toInt()
     }
+//        3.4
+         val volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
     fun printSize(){
         println("Width: $width cm " + "Length: $length cm " + "Height: $height cm ")
+          println("Volume: $volume litres")
     }
 }
