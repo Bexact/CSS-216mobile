9dad2cd58d55671246cbaab8b7526e5208452e38
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-11 22:02:31 +0600

    3.4

--- Hello Kotlin/src/myapp/Main.kt
+++ Hello Kotlin/src/myapp/Main.kt
@@ -25,7 +25,8 @@ package example.myapp
 fun buildAquarium() {
     val aquarium6 = Aquarium(29)
     aquarium6.printSize()
-    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+//     3.4
+//     println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 }
 
 fun main(){
