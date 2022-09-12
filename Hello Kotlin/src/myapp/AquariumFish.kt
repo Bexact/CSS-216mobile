76001e25609cc998cc6c2f563ef84afefe793265
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-12 22:55:23 +0600

    6.2

--- Hello Kotlin/src/myapp/AquariumFish.kt
+++ Hello Kotlin/src/myapp/AquariumFish.kt
@@ -3,11 +3,19 @@ package example.myapp
 abstract class AquariumFish {
     abstract val color: String
 }
-
-class Shark: AquariumFish() {
+interface FishAction{
+    fun eat()
+}
+class Shark: AquariumFish(), FishAction {
     override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
 }
 
-class Plecostomus: AquariumFish() {
+class Plecostomus: AquariumFish(), FishAction {
     override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
 }
