8ccf797de78e8a27a4aac9ae0754f04bb9cf55a0
Bexact <78924792+Bexact@users.noreply.github.com>
2022-09-12 23:28:38 +0600

    7

--- Hello Kotlin/src/myapp/AquariumFish.kt
+++ Hello Kotlin/src/myapp/AquariumFish.kt
@@ -1,21 +1,49 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
-}
-interface FishAction{
+// abstract class AquariumFish {
+//     abstract val color: String
+// }
+// interface FishAction{
+//     fun eat()
+// }
+// class Shark: AquariumFish(), FishAction {
+//     override val color = "grey"
+//     override fun eat() {
+//         println("hunt and eat fish")
+//     }
+// }
+
+// class Plecostomus: AquariumFish(), FishAction {
+//     override val color = "gold"
+//     override fun eat() {
+//         println("eat algae")
+//     }
+// }
+// 7
+interface FishAction {
     fun eat()
 }
-class Shark: AquariumFish(), FishAction {
+
+interface FishColor {
+    val color: String
+}
+
+class Plecostomus (fishColor: FishColor = GoldColor):
+        FishAction by PrintingFishAction("eat algae"),
+        FishColor by fishColor
+
+class Shark: FishAction, FishColor {
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
-
-class Plecostomus: AquariumFish(), FishAction {
+object GoldColor : FishColor {
     override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
     override fun eat() {
-        println("eat algae")
+        println(food)
     }
 }
